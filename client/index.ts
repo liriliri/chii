7b57e7a0108dbf2fd886a1afb6356de6c40e08e6
surunzi <redhoodsu@tencent.com>
2020-04-20 21:11:51 +0800

    chore: init ws

--- client/index.ts
+++ client/index.ts
@@ -1,1 +1,7 @@
-console.log('client')
+const ws = new WebSocket(`ws://${location.host}/foo`);
+
+ws.addEventListener('open', () => {
+  setInterval(() => {
+    ws.send('hello world');
+  }, 1000);
+});
--- server/index.js
+++ server/index.js
@@ -1,13 +1,31 @@
 const Koa = require('koa');
+const url = require('url');
+const WebSocket = require('ws');
 
 const router = require('./middle/router');
 
 function start(port = 3000) {
+  const wss = new WebSocket.Server({ noServer: true });
+  wss.on('connection', ws => {
+    ws.on('message', message => console.log(message));
+  });
+
   const app = new Koa();
 
   app.use(router());
 
-  app.listen(port);
+  const server = app.listen(port);
+  server.on('upgrade', function (request, socket, head) {
+    const pathname = url.parse(request.url).pathname;
+
+    if (pathname === '/foo') {
+      wss.handleUpgrade(request, socket, head, ws => {
+        wss.emit('connection', ws, request);
+      });
+    } else {
+      socket.destroy();
+    }
+  });
 }
 
 module.exports = {
