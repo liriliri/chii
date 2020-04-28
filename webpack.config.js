cfaa015861d4602e291e0f62e04f7df6ee488665
surunzi <redhoodsu@tencent.com>
2020-04-28 20:43:40 +0800

    chore: small changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,29 @@
 const path = require('path');
 
-module.exports = {
-  entry: './target/index.ts',
-  devtool: 'inline-source-map',
-  output: {
-    filename: 'target.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  resolve: {
-    extensions: ['.ts', '.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-      },
-    ],
-  },
+module.exports = (env, argv) => {
+  const config = {
+    entry: './target/index.ts',
+    devtool: 'inline-source-map',
+    output: {
+      filename: 'target.js',
+      path: path.resolve(__dirname, 'public'),
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+        },
+      ],
+    },
+  };
+
+  if (argv.mode === 'production') {
+    config.devtool = 'none';
+  }
+
+  return config;
 };
