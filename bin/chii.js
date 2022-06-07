5b4e2f4b5a5ed96fe4b11a8f8bdfd1f7700ae2d5
redhoodsu <redhoodsu@tencent.com>
2022-06-07 19:29:21 +0800

    chore: small changes

--- bin/chii.js
+++ bin/chii.js
@@ -8,7 +8,7 @@ program.version(version);
 
 program
   .command('start')
-  .description('starts chi server')
+  .description('starts chii server')
   .option('-p, --port <port>', 'set the port to start on. defaults to 3000', parseInt)
   .option('-h, --host <host>', 'set the host. defaults to 0.0.0.0')
   .option('-d, --domain <domain>', 'set the domain. defaults to localhost:port')
