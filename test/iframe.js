dbc3d1627edfcc75cdd32a4b11cbdfda7a3b1e49
redhoodsu <redhoodsu@tencent.com>
2023-04-01 12:56:38 +0800

    chore: small changes

--- test/iframe.js
+++ test/iframe.js
@@ -29,7 +29,6 @@ function load() {
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="iframe.css">
       <title>Target</title>
       <style>
         .motto {
